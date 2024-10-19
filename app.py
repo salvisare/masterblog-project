83e305217db572e2e2fe0e343f46bc61642eb92d
Salvis Are <salvis.are@gmail.com>
2024-10-19 11:09:44 +0200

    json blog posts data, index tamplate and static css file

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import json
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+# Load blog posts from JSON file
+def load_blog_posts():
+    try:
+        with open('blog_posts.json', 'r') as file:
+            return json.load(file)
+    except json.JSONDecodeError as e:
+        print(f"Failed to decode JSON: {e}")
+        return []  # Return an empty list in case of error
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+        return []  # Return an empty list if file is missing
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    blog_posts = load_blog_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 @app.route('/about')
